0b62e7e85d665039986a4118fc45b3a79c398435
Ahmed Kadri <kadri.ahmedbaha@gmail.com>
2025-01-18 20:12:29 +0100

    .

--- streamlit/app.py
+++ streamlit/app.py
@@ -2,16 +2,21 @@ import pyvista as pv
 import streamlit as st
 from stpyvista import stpyvista
 
-st.title("3D Model Visualization")
 
-# Initialize a PyVista plotter object
-plotter = pv.Plotter(window_size=[800, 600])
+if __name__ == "__main__":
 
-# Create a sample mesh (e.g., a cube)
-mesh = pv.Cube()
+    pv.start_xvfb()
+    
+    st.title("3D Model Visualization")
 
-# Add the mesh to the plotter
-plotter.add_mesh(mesh, color='lightblue')
+    # Initialize a PyVista plotter object
+    plotter = pv.Plotter(window_size=[800, 600])
 
-# Display the plotter in Streamlit
-stpyvista(plotter, key="pv_cube")
+    # Create a sample mesh (e.g., a cube)
+    mesh = pv.Cube()
+
+    # Add the mesh to the plotter
+    plotter.add_mesh(mesh, color='lightblue')
+
+    # Display the plotter in Streamlit
+    stpyvista(plotter, key="pv_cube")
